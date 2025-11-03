1239b2f11101579af1f8a6f2798b07f844c40e20
Mohammed Tangalvadi <mtangalv@student.42abudhabi.ae>
2025-11-03 11:13:22 +0400

    add ex02

---
+++ cpp-01/ex02/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string	msg = "HI THIS IS BRAIN";
+	std::string *stringPTR = &msg;
+	std::string &stringREF = msg;
+
+	std::cout << &msg << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << msg << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+}
