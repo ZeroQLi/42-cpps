4dbc4fead0e890b326c245cc887a71cb5ee6f95b
Mohammed Eshan <itsrandomboy77@gmail.com>
2026-04-29 14:35:33 +0400

    finish ex00

--- cpp-07/ex01/iter.hpp
+++ cpp-07/ex01/iter.hpp
@@ -3,6 +3,11 @@
 
 #include <iostream>
 
-template <typename T>
+template <typename Tarray, typename Tfunction>
+void iter(Tarray *arr, const size_t len, Tfunction func)
+{
+	for (size_t i = 0; i < len; i++)
+		func(arr[i]);
+}
 
 #endif
--- cpp-07/ex01/main.cpp
+++ cpp-07/ex01/main.cpp
@@ -1,6 +1,29 @@
 #include "./iter.hpp"
+#include <string>
+
+void	square(int &num)
+{
+	num *= num;
+}
+
+void	to_upper(std::string &ref)
+{
+	for (size_t i = 0; i < ref.length(); i++)
+		ref[i] = std::toupper(ref[i]);
+}
 
 int main(void)
 {
-	
+	int arr[] = {0, 1, 2, 3, 4, 5};
+	std::string strs[] = {"hello", "HI", "My", "n_a_m_e"};
+
+	::iter(arr, 6, square);
+	for (int i = 0; i < 6; i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+
+	::iter(strs, 4, to_upper);
+	for (int i = 0; i < 4; i++)
+		std::cout << strs[i] << " ";
+	std::cout << std::endl;
 }
