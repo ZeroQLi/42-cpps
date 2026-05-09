f1fb566ad1196c87e4aecabfb027510c40e0e962
Mohammed Eshan <itsrandomboy77@gmail.com>
2026-05-09 16:32:46 +0400

    add a extra print in ex01

--- cpp-06/ex01/src/main.cpp
+++ cpp-06/ex01/src/main.cpp
@@ -7,10 +7,18 @@ int	main(void)
 	data->rank = 5;
 	data->status = true;
 
+	std::cout << "Original: \n"
+			  << data->name << "\n"
+			  << data->rank << "\n"
+			  << (data->status ? "true" : "false") << "\n\n";
+
 	uintptr_t	serialized = Serializer::serialize(data);
-	std::cout << "Serialized: " << serialized << std::endl;
+	std::cout << "Serialized: " << serialized << "\n\n";
 
 	Data	*deserialized = Serializer::deserialize(serialized);
-	std::cout << "Deserialized: \n" << deserialized->name << "\n" << deserialized->rank << "\n" << (deserialized->status ? "true" : "false") << std::endl;
+	std::cout << "Deserialized: \n" 
+			  << deserialized->name << "\n" 
+			  << deserialized->rank << "\n" 
+			  << (deserialized->status ? "true" : "false") << std::endl;
 	return (0);
 }
